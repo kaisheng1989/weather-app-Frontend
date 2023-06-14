5879e35c05f7eab30d7d375473f031cba88b94c4
kaisheng1989 <kaisheng1989@gmail.com>
2023-06-14 11:50:15 +0800

    Adding commit for code for better understanding

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -9,6 +9,7 @@ function Login(props) {
       </div>
       {/*Taking full width in desktop and hidden when in mobile */}
       <div className="hidden relative lg:flex h-full w-1/2 items-center justify-center bg-gray-200 ">
+         {/* Animation for the Icon to bounce resembling a sun rise. */}
         <div
           className="w-60 h-60 bg-gradient-to-tr
       from-blue-400 to-purple-800 rounded-full animate-bounce"
